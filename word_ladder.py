3edeaa2e38ce846cee940b03746340cad071c813
Jordan Schurmann <Jordan.Schurmann@griffithuni.edu.au>
2017-08-06 08:48:51 +1000

    - Added ability to add a file of words to remove from the path and consolidated some of the validation

--- word_ladder.py
+++ word_ladder.py
@@ -4,11 +4,11 @@ import re
 # application information
 __author__ = "Jordan Schurmann, Luke Reynolds"
 __email__ = "jordan.schurmann@gmail.com, lreynolds188@gmail.com"
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __website__ = "http://lukereynolds.net/"
 
 
-# return the number of letters that match
+# return the number of letters for each item that match that target word
 def same(item, target):
     return len([c for (c, t) in zip(item, target) if c == t])
 
@@ -23,12 +23,14 @@ def find(start, words, seen, target, path):
     list = []
     # for each letter in the starting word
     for i in range(len(start)):
-        # add the return value of the build function to the list when sent the pattern of the word (e.g. lead: ".ead", "l.ad", "le.d", "lea.")
+        # add the return value of the build function to the list when sent the pattern of the word
+        # (e.g. lead: ".ead", "l.ad", "le.d", "lea.")
         list += build(start[:i] + "." + start[i + 1:], words, seen, list)
     # if the list is empty
     if len(list) == 0:
         return False
-    # sort the list into pairs showing the current words closeness to the target word followed by the current word (e.g. the word 'load' and 'gold' have 2 of the same letters so it will be represented by (2, 'load'))
+    # sort the list into pairs showing the current words closeness to the target word followed by the
+    # current word (e.g. the word 'load' and 'gold' have 2 of the same letters so it will be represented by (2, 'load'))
     list = sorted([(same(w, target), w) for w in list], reverse=short)
     # for each pair in the list
     for (match, item) in list:
@@ -41,7 +43,11 @@ def find(start, words, seen, target, path):
             return True
         # mark that the word has been looked at
         seen[item] = True
-    # not really sure
+
+    # !!!!!!! If not found then for each item in the list iterate to find the path to target
+    # !!!!!!! This code is wrong as it choses one path and does and finds its end. It does not go back
+    # !!!!!!! and compare if this is necessarily the shortest path.   Jordan S.  Need to look at this.
+    # !!!!!!! Eg Hide -> Seek should be 6 steps but this is done in 19 steps.
     for (match, item) in list:
         path.append(item)
         if find(item, words, seen, target, path):
@@ -49,7 +55,7 @@ def find(start, words, seen, target, path):
         path.pop()
 
 
-# validate file
+# Validate input file
 def valid_file(fname):
     try:
         if os.stat(fname).st_size > 0:  # if filename contains data
@@ -60,7 +66,7 @@ def valid_file(fname):
         return 2
 
 
-# validate start word
+# Validate input for start word
 def valid_start(start, lines):
     if start.isalpha():  # start word must be alphabetic
         if len(start) > 1:  # start word must be larger than 1 character
@@ -74,7 +80,7 @@ def valid_start(start, lines):
         return 3
 
 
-# validate target word
+# Validate input for target word
 def valid_target(start, target, words):
     if target.isalpha():  # target word must be alphabetic
         if len(start) == len(target):  # target word must be same size as start word
@@ -91,13 +97,13 @@ def valid_target(start, target, words):
         return 4
 
 
-# validate input for shortest path
-def valid_path(shortest):
+# Validate input y/n
+def valid_yn(flag):
     yes_or_no = ['y', 'n']
-    if len(shortest) == 1:
-        if shortest.isalpha():
-            if shortest in yes_or_no:
-                if shortest == 'y':
+    if len(flag) == 1:
+        if flag.isalpha():
+            if flag in yes_or_no:
+                if flag == 'y':
                     return 'y'
                 else:
                     return 'n'
@@ -105,26 +111,56 @@ def valid_path(shortest):
                 return 1
         else:
             return 2
-    elif len(shortest) > 1:
+    elif len(flag) > 1:
         return 3
     else:
         return 4
 
 
-# get file name
+# Get input dictionary
 while True:
-    fname = input("Enter dictionary name: ").lower()
+    fname = (input("Enter dictionary name: ").lower()).strip()
     file_error = valid_file(fname)
     if file_error == 0:
+        # open and read file
+        lines = (open(fname, 'r').read()).split()
         break
     elif file_error == 1:
         print("Selected file is empty....please reenter")
     else:
         print("Can not find the file....please reenter")
 
-# open corresponding dictionary
-lines = (open(fname, 'r').read()).split()
-# get start word
+# Get not used words
+while True:
+    flag = (input("Would you like to supply a list of words not to use? y/n: ").lower()).strip()
+    yn_error = valid_yn(flag)
+    notwords =[]
+    if yn_error == 'y':
+        while True:
+            fname = (input("Enter name of file containing characters not to be used: ").lower()).strip()
+            file_error = valid_file(fname)
+            if file_error == 0:
+                # open and read file
+                notwords = (open(fname, 'r').read()).split()
+                break
+            elif file_error == 1:
+                print("Selected file is empty....please reenter")
+            else:
+                print("Can not find the file....please reenter")
+        break
+    elif yn_error == 'n':
+        break
+    elif yn_error == 1:
+        print("Please enter Y or N only")
+    elif yn_error == 2:
+        print("Please enter letters Y or N only")
+    elif yn_error == 3:
+        print("Please enter only one character")
+    else:
+        print("Please enter a character")
+
+
+# Get start word
 while True:
     start = (input("Enter start word: ").lower()).strip()
     start_error = valid_start(start, lines)
@@ -137,14 +173,16 @@ while True:
     else:
         print("Start word must contain only letters....please reenter")
 
-# create an array and fill it with words from the list with the same length as the starting word
+# Create an array and fill it with words from the list with the same length as the starting word
+# notwords will be blank if the user has decided not to supply any words to omit
 words = []
 for line in lines:
     word = line.rstrip()
     if len(word) == len(start):
-        words.append(word)
+        if word not in notwords:
+            words.append(word)
 
-# get target word
+# Get target word
 while True:
     target = (input("Enter target word: ").lower()).strip()
     target_error = valid_target(start, target, words)
@@ -159,24 +197,24 @@ while True:
     else:
         print("Target word must contain only letters....please reenter")
 
-# Shortest or longest path
+# Get shortest or longest path
+short = False
 while True:
-    shortest = (input("Would you like the short route? y/n: ").lower()).strip()
-    path_error = valid_path(shortest)
-    if path_error == 'y':
+    flag = (input("Would you like the short route? y/n: ").lower()).strip()
+    yn_error = valid_yn(flag)
+    if yn_error == 'y':
         short = True
         break
-    elif path_error == 'n':
+    elif yn_error == 'n':
         break
-    elif path_error == 1:
+    elif yn_error == 1:
         print("Please enter Y or N only")
-    elif path_error == 2:
+    elif yn_error == 2:
         print("Please enter letters Y or N only")
-    elif path_error == 3:
+    elif yn_error == 3:
         print("Please enter only one character")
     else:
         print("Please enter a character")
-
 
 count = 0
 path = [start]
