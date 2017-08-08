8ec4c145edfbf9c89cde607913c152cf27f816b6
Jordan Schurmann <Jordan.Schurmann@griffithuni.edu.au>
2017-08-08 19:22:16 +1000

    - Removed redundant comments - Removed error list function as needlessly complicated

--- word_ladder.py
+++ word_ladder.py
@@ -4,7 +4,7 @@ import re
 # application information
 __author__ = "Jordan Schurmann, Luke Reynolds"
 __email__ = "jordan.schurmann@gmail.com, lreynolds188@gmail.com"
-__version__ = "1.0.15"
+__version__ = "1.0.16"
 __website__ = "http://lukereynolds.net/"
 
 
@@ -18,9 +18,7 @@ def same(item, target):
     :type target: str
     :return: Integer of the number of letter matches for the word
     :rtype: int
-
     """
-
     return len([c for (c, t) in zip(item, target) if c == t])
 
 
@@ -32,15 +30,13 @@ def build(pattern, words, seen, list):
     :type pattern: str
     :param words: List of all words
     :type words: list
-    :param seen: Dictionary with words and count of letter matches for each word
+    :param seen: Dictionary with words and a value of true if they have been identified
     :type seen: dict
-    :param list: List of currently viewed words - don't want to reuse them
+    :param list: List of words that match the current pattern and are not in seen
     :type list: list
     :return: Words matching pattern
     :rtype: list
-
     """
-
     return [word for word in words
             if re.search(pattern, word) and word not in seen.keys() and word not in list]
 
@@ -61,36 +57,27 @@ def find(start, words, seen, target, path):
     :type path: List
     :return: True if found or False if no path for current item
     :rtype: Boolean
-
     """
-
     matchedletters = []
     list = []
 
-    # Determine whether start and target letters match. If greater than 0 then identify matching letters so that
-    # these letters are excluded from the new pattern. All new words are to have these letters at a minimum.
+    # Determine whether start and target letters match. Exclude matching letters from pattern.
     if (sum(1 for (c, t) in zip(start, target) if c == t)) > 0:
-        for i in range(len(start)):
-            # Compare the new start word to the target word and return a list containing indexes
-            # of matched letters.  E.g. [0,6] indicates letter matches at the first and seventh letters.
-            matchedletters = [i for i, x in enumerate(zip(start, target)) if all(y == x[0] for y in x)]
+        # Compare the new start word to the target word and return a list containing indexes of matched letters
+        matchedletters = [i for i, x in enumerate(zip(start, target)) if all(y == x[0] for y in x)]
     for i in range(len(start)):
         # For each letter position, only build a pattern for letter positions that have not already been found.
         if i not in matchedletters:
-            # Add all words matching the pattern to the list. Only process patterns where the letter
-            # has not been found. Pattern is of type lead: ".ead", "l.ad", "le.d", "lea.")
+            # Add all words matching the pattern to the list. Only process patterns where the letter has not been found
             list += build(start[:i] + "." + start[i + 1:], words, seen, list)
 
     # If the list is empty (no matching words found) - Exit the iteration and process the next item if one exists.
-    # If no other item exists then exit as No Path Found.
     if len(list) == 0:
         return False
 
-    # Sort the list by number of matches and alphabetically.  If reverse is specified sort by
-    # highest number of matching letters first and then alphabetically.
+    # Sort the list by number of matches and alphabetically.  If reverse is specified sort by matched letters first
     list = sorted([(same(w, target), w) for w in list], reverse=short)
 
-    # For each matched pair in the sorted list.
     for (match, item) in list:
 
         # If the current word matches target or 1 letter off target.
@@ -99,7 +86,7 @@ def find(start, words, seen, target, path):
                 # Append the word to the path and exit the recursion, as we have found the last word.
                 path.append(item)
             return True
-        # For each word in the current list add it too seen so that future searches exclude the word.
+        # For each word in the current list mark it true so that future word searches exclude the word.
         seen[item] = True
 
     # Set the first word in the list to be the new start word to iterate on.
@@ -119,18 +106,16 @@ def valid_file(fname):
 
     :param fname: File name input
     :type fname: str
-    :return: Integer indicating whether file data is valid
-    :rtype: int
-
+    :return: String indicating whether file data is valid
+    :rtype: str
     """
-
     try:
         if os.stat(fname).st_size > 0:  # if filename contains data
-            return 0
+            return "0"
         else:
-            return 1
-    except OSError:  # if no file of that name found
-        return 2
+            return "Selected file is empty....please reenter"
+    except OSError:
+        return "Can not find the file....please reenter"
 
 
 def valid_start(start, lines):
@@ -141,21 +126,19 @@ def valid_start(start, lines):
     :type start: str
     :param lines: List of dictionary words
     :type lines: list
-    :return: Integer indicating whether data is valid
-    :rtype: int
-
+    :return: String indicating whether data is valid
+    :rtype: str
     """
-
     if start.isalpha():  # start word must be alphabetic
         if len(start) > 1:  # start word must be larger than 1 character
             if start in lines:  # start word must be in the list of words
-                return 0
+                return "0"
             else:
-                return 3
+                return "Start word not in list of words....please reenter"
         else:
-            return 4
+            return "Start word must contain more than one letter....please reenter"
     else:
-        return 5
+        return "Start word must contain only letters....please reenter"
 
 
 def valid_target(start, target, words):
@@ -168,24 +151,22 @@ def valid_target(start, target, words):
     :type target: str
     :param words: List of dictionary words
     :type words: list
-    :return: Integer indicating whether data is valid
-    :rtype: int
-
+    :return: String indicating whether data is valid
+    :rtype: str
     """
-
     if target.isalpha():  # target word must be alphabetic
         if len(start) == len(target):  # target word must be same size as start word
             if start != target:  # target and start words must be different
                 if target in words:  # target word must be in the list of words
-                    return 0
+                    return "0"
                 else:
-                    return 6
+                    return "Target word not in list of words....please reenter"
             else:
-                return 7
+                return "Target word must be different from Start word....please reenter"
         else:
-            return 8
+            return "Target word must be same length as Start word....please reenter"
     else:
-        return 9
+        return "Target word must only contain letters....please renter"
 
 
 def valid_yn(flag):
@@ -194,109 +175,67 @@ def valid_yn(flag):
 
     :param flag: String indicating yes or no
     :type flag: str
-    :return: Integer indicating whether data is valid
-    :rtype: int
-
+    :return: String indicating whether data is valid
+    :rtype: str
     """
-
     yes_or_no = ['y', 'n']
     if len(flag) == 1:  # flag must be one character
         if flag.isalpha():  # flag must be alphabetic
             if flag in yes_or_no:  # target word must be Y or N
                 if flag == 'y':
-                    return 0  # 0 = y
+                    return "y"
                 else:
-                    return 99  # 99 = n
+                    return "n"
             else:
-                return 10
+                return "Please enter Y or N only"
         else:
-            return 11
+            return "Please enter letters Y or N only"
     elif len(flag) > 1:
-        return 12
+        return "Please enter only one character"
     else:
-        return 13
+        return "Please enter a character"
 
-
-def errormessage(error):
-    """
-    Function to return error message depending upon error code provided.
-
-    :param error: Error code received from the various validation functions
-    :type error: int
-    :return: Error message for the integer
-    :rtype: str
-
-    """
-    message = ""
-    if error == 1:
-        message = "Selected file is empty....please reenter"
-    elif error == 2:
-        message = "Can not find the file....please reenter"
-    elif error == 3:
-        message = "Start word not in list of words....please reenter"
-    elif error == 4:
-        message = "Start word must contain more than one letter....please reenter"
-    elif error == 5:
-        message = "Start word must contain only letters....please reenter"
-    elif error == 6:
-        message = "Target word not in list of words....please reenter"
-    elif error == 7:
-        message = "Target word must be different from Start word....please reenter"
-    elif error == 8:
-        message = "Target word must be same length as Start word....please reenter"
-    elif error == 9:
-        message = "Target word must only contain letters....please renter"
-    elif error == 10:
-        message = "Please enter Y or N only"
-    elif error == 11:
-        message = "Please enter letters Y or N only"
-    elif error == 12:
-        message = "Please enter only one character"
-    elif error == 13:
-        message = "Please enter a character"
-    return message
 
 # Get input dictionary file
 while True:
     fname = (input("Enter dictionary name: ").lower()).strip()
     file_error = valid_file(fname)
-    if file_error == 0:
-        lines = (open(fname, 'r').read()).split()  # open and read file
+    if file_error == "0":
+        lines = (open(fname, 'r').read()).split()
         break
     else:
-        print(errormessage(file_error))
+        print(file_error)
 
 # Get input excluded words file
 while True:
     flag = (input("Would you like to supply a list of words not to use? y/n: ").lower()).strip()
     yn_error = valid_yn(flag)
     excluded = []
-    if yn_error == 0:  # Yes
+    if yn_error == "y":
         while True:
             fname = (input("Enter name of file containing characters not to be used: ").lower()).strip()
             file_error = valid_file(fname)
-            if file_error == 0:
-                excluded = (open(fname, 'r').read()).split()  # open and read file
+            if file_error == "0":
+                excluded = (open(fname, 'r').read()).split()
                 break
             else:
-                print(errormessage(file_error))
+                print(file_error)
         break
-    elif yn_error == 99:  # No
+    elif yn_error == "n":
         break
     else:
-        print(errormessage(yn_error))
+        print(yn_error)
 
 # Get input start word
 while True:
     start = (input("Enter start word: ").lower()).strip()
     start_error = valid_start(start, lines)
-    if start_error == 0:
+    if start_error == "0":
         break
     else:
-        print(errormessage(start_error))
+        print(start_error)
 
 # Create an array and fill it with words from the list with the same length as the starting word
-# excluded will be blank if the user has decided not to supply any words to omit
 words = []
 for line in lines:
     word = line.rstrip()
@@ -308,23 +247,23 @@ for line in lines:
 while True:
     target = (input("Enter target word: ").lower()).strip()
     target_error = valid_target(start, target, words)
-    if target_error == 0:
+    if target_error == "0":
         break
     else:
-        print(errormessage(target_error))
+        print(target_error)
 
 # Get input shortest or longest path
 short = False
 while True:
     flag = (input("Would you like the short route? y/n: ").lower()).strip()
     yn_error = valid_yn(flag)
-    if yn_error == 0:  # Yes
+    if yn_error == "y":
         short = True
         break
-    elif yn_error == 99:  # No
+    elif yn_error == "n":
         break
     else:
-        print(errormessage(yn_error))
+        print(yn_error)
 
 count = 0
 path = [start]
